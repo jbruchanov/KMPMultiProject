58ba18f6faeb70b2c07b8e291306f2b3291ebeae
Jiri Bruchanov <jbruchanov@gmail.com>
2025-08-24 22:01:04 +0100

    Add IME issue example

--- AppProject/shared/src/commonMain/kotlin/com/example/appproject/App.kt
+++ AppProject/shared/src/commonMain/kotlin/com/example/appproject/App.kt
@@ -1,12 +1,25 @@
 package com.example.appproject
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,29 +27,54 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        val greeting = remember { Greeting().greet() }
-        Column(
-            Modifier
-                .safeDrawingPadding()
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(
+            modifier = Modifier
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+                    .background(Color.Red.copy(0.1f))
+                    .safeDrawingPadding()
+                    .background(Color.LightGray)
+                    .padding(16.dp)
+
+            ) {
+
+                repeat(10) {
+                    var text by remember { mutableStateOf("$it") }
+                    TextField(
+                        value = text,
+                        onValueChange = { text = it },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(60.dp))
+                }
             }
-            AnimatedVisibility(showContent) {
-                Column(
-                    Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+
+            val bottomIme = WindowInsets.ime.only(WindowInsetsSides.Bottom)
+            if (bottomIme.getBottom(LocalDensity.current) > 0f) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter)
+                        .padding(bottomIme.asPaddingValues())
                 ) {
-                    Text("Greeting: $greeting")
+                    val keyboardController = LocalSoftwareKeyboardController.current
+                    Button(onClick = { keyboardController?.hide() }) {
+                        Text("Done")
+                    }
                 }
             }
         }
